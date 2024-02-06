6faad1bc9dadad7f88ae2a943ff11b50136be31d
Timur Turatbekov <timatur242005@gmail.com>
2024-02-06 13:39:36 +0300

    Removed unused _sign

--- lib/src/lat_lng_triangle.dart
+++ lib/src/lat_lng_triangle.dart
@@ -13,11 +13,6 @@ class LatLngTriangle {
         (a.longitude + b.longitude + c.longitude) / 3,
       );
 
-  double _sign(LatLng p1, LatLng p2, LatLng p3) {
-    return (p1.latitude - p3.latitude) * (p2.longitude - p3.longitude) -
-        (p2.latitude - p3.latitude) * (p1.longitude - p3.longitude);
-  }
-
   @override
   operator ==(Object other) {
     if (identical(this, other)) return true;
